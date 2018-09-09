9851d67faf46082de006fac7a8b98cf386d33194
unknown <guptaachin@rocketmail.com>
2018-09-09 18:15:29 +0100

    adding other files

--- cleaning_helper.py
+++ cleaning_helper.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
 
+
+# this was a low hanging fruit. Replacing the None values with most frequent embarking station.
+def fix_embarked(dataframe):
+    dataframe.loc[61, "Embarked"] = 'S'
+
+
 # categorizing the age field. Continuous to Categorical
 def age_to_categorical(dataframe):
     cuts = pd.cut(dataframe['Age'], bins=10)
@@ -10,6 +16,9 @@ def age_to_categorical(dataframe):
         if e not in hash_map:
             hash_map[e] = index_
             index_ += 1
+
+    # replacing the intervals with respective discrete values.
+    # fixing the NaNs.
 
     new_list = []
     for e in cuts:
